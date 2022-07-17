0ca7609bd7af31b1b1d442c3f358d845953f5079
B-loeb <bradleyloeb@gmail.com>
2022-07-17 14:07:15 -0400

    moved folder

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-require ('dotenv').config()
+require('dotenv').config()
 const express = require('express')
-const { get } = require('http')
 const app = express()
 
-app.use('places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello cruel world!')
+    res.send('Hello world!')
 })
 //wildcard route stays at bottom
-app get('*', (req, res) => {
+app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
+
 app.listen(process.env.PORT)
