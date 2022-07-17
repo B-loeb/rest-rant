717ec4072b4abd9ce1118eff3bcbccad60f8489a
B-loeb <bradleyloeb@gmail.com>
2022-07-17 13:43:25 -0400

    add 404

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+require ('dotenv').config()
 const express = require('express')
+const { get } = require('http')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello cruel world!')
 })
-
-app.listen(3000)
+//wildcard route stays at bottom
+app get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+app.listen(process.env.PORT)
