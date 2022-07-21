c1b548c1fa746b423dfbda39d6cb29299ac1fb73
B-loeb <bradleyloeb@gmail.com>
2022-07-21 16:00:22 -0400

    added home page view

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+
+// defines the view engine
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //import rouuter code from controller
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
 //wildcard route stays at bottom
 app.get('*', (req, res) => {
