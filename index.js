ca5ee66ba4c9ea12f6a2ded5afccc973ce7476e0
B-loeb <bradleyloeb@gmail.com>
2022-07-26 16:24:46 -0400

    Form data came through

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 //import router code from controller
 app.use('/places', require('./controllers/places'))
+
 
 //home route
 app.get('/', (req, res) => {
