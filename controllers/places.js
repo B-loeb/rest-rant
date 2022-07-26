ad6cd8bd6fc1ffc53c7c78fdf6f0b03dd47268dc
B-loeb <bradleyloeb@gmail.com>
2022-07-26 19:01:00 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -25,7 +25,17 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 router.get('/:id', (req, res) => {
- //look at form with correct code not posted
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
 })
+
 
 module.exports = router
