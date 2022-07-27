df0b74b5dc015bda42d97dc91afb88bfe0eb861e
B-loeb <bradleyloeb@gmail.com>
2022-07-26 21:47:31 -0400

    Delete Fuctionality implemented

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 //globals
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
-
 // defines the view engine
+//app.set('views', _dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 //import router code from controller
 app.use('/places', require('./controllers/places'))
 
